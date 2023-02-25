bcc505a0df78cf97803e22383db8f53691ec650f
SherinJM <115409980+SherinJM@users.noreply.github.com>
2023-02-25 19:57:03 +0530

    Category test added

---
+++ src/test/java/testcases/CreateCategory.java
@@ -0,0 +1,12 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class CreateCategory {
+
+	@Test
+	public static void createCategory() {
+		System.out.println("Category added successfully");
+	}
+
+}
